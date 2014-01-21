12e5bfdf30c3643ab6dcd162890c78636275286b
Povilas Kanapickas <povilas@radix.lt>
2014-01-21 09:21:17 +0200

    Add libwnck-wrapper.h which was accidentally matched by .gitignore

---
+++ wnck/wnckmm/libwnck-wrapper.h
@@ -0,0 +1,26 @@
+/** This file is part of wnckmm
+
+    Copyright (c) 2013 Povilas Kanapickas  <povilas@radix.lt>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef LIBWNCKMM_LIBWNCK_WRAPPER_H
+#define LIBWNCKMM_LIBWNCK_WRAPPER_H
+
+#define WNCK_I_KNOW_THIS_IS_UNSTABLE
+
+#include <libwnck/libwnck.h>
+
+#endif
